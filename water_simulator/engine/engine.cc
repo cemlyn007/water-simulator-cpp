58a89525a7c9d639baaf9a0ad6a3fc09bb75a7ee
Cemlyn Neil Waters <cemlynwaters@gmail.com>
2025-03-01 12:29:02 +0000

    perf: optimize frame rate

--- water_simulator/engine/engine.cc
+++ water_simulator/engine/engine.cc
@@ -1,4 +1,5 @@
 #include "water_simulator/engine/engine.h"
+#include <algorithm>
 #include <cassert>
 #include <cmath>
 #include <mdspan>
@@ -51,17 +52,17 @@ const std::vector<float> sphere_body_heights(const std::vector<float> &sphere_ce
   return body_heights;
 }
 
-const std::vector<float> cross_correlation(std::span<float> input, const std::vector<float> &kernel,
-                                           const size_t input_n, const size_t input_m, const size_t kernel_n,
-                                           const size_t kernel_m) {
+void cross_correlation(std::span<float> output, const std::span<float> input, const std::array<float, 9> &kernel,
+                       const size_t input_n, const size_t input_m) {
+  const size_t kernel_n = 3;
+  const size_t kernel_m = 3;
   if (input.size() != input_n * input_m)
     throw std::runtime_error("Invalid input size");
-  if (kernel.size() != kernel_n * kernel_m)
-    throw std::runtime_error("Invalid kernel size");
-  std::vector<float> output(input.size(), 0);
+  if (output.size() != input.size())
+    throw std::runtime_error("Invalid output size");
   for (size_t i = 0; i < input_n; ++i) {
     for (size_t j = 0; j < input_m; ++j) {
-      const size_t update_index = i * input_m + j;
+      float output_element = 0.0;
       for (size_t ki = 0; ki < kernel_n; ++ki) {
         size_t get_i = i + ki - kernel_n / 2;
         if (i + ki < kernel_n / 2) {
@@ -76,26 +77,57 @@ const std::vector<float> cross_correlation(std::span<float> input, const std::ve
           } else if (get_j >= input_m) {
             get_j = input_m - 1;
           }
-          output[update_index] += input[get_i * input_m + get_j] * kernel[ki * kernel_m + kj];
+          output_element += input[get_i * input_m + get_j] * kernel[ki * kernel_m + kj];
         }
       }
+      output[i * input_m + j] = output_element;
     }
   }
-  return output;
 }
 
-std::vector<float> smooth_sphere_body_heights(std::vector<float> body_heights, const size_t n_spheres, const size_t n,
-                                              const size_t m) {
-  const std::vector<float> kernel(9, 1.0f / 9.0f);
-  auto span_body_heights = std::span(body_heights);
-  for (size_t index = 0; index < 2; ++index) {
-    for (size_t sphere = 0; sphere < n_spheres; ++sphere) {
-      auto sphere_body_heights = span_body_heights.subspan(sphere * n * m, n * m);
-      const auto new_sphere_body_heights = cross_correlation(sphere_body_heights, kernel, n, m, 3, 3);
-      std::copy(new_sphere_body_heights.begin(), new_sphere_body_heights.end(), sphere_body_heights.begin());
+void cross_correlation(std::vector<float> &output, const std::span<float> input, const std::array<float, 9> &kernel,
+                       const size_t input_n, const size_t input_m) {
+  const size_t kernel_n = 3;
+  const size_t kernel_m = 3;
+  if (input.size() != input_n * input_m)
+    throw std::runtime_error("Invalid input size");
+  if (output.size() != input.size())
+    throw std::runtime_error("Invalid output size");
+  for (size_t i = 0; i < input_n; ++i) {
+    for (size_t j = 0; j < input_m; ++j) {
+      float output_element = 0.0;
+      for (size_t ki = 0; ki < kernel_n; ++ki) {
+        size_t get_i = i + ki - kernel_n / 2;
+        if (i + ki < kernel_n / 2) {
+          get_i = 0;
+        } else if (get_i >= input_n) {
+          get_i = input_n - 1;
+        }
+        for (size_t kj = 0; kj < kernel_m; ++kj) {
+          size_t get_j = j + kj - kernel_m / 2;
+          if (j + kj < kernel_m / 2) {
+            get_j = 0;
+          } else if (get_j >= input_m) {
+            get_j = input_m - 1;
+          }
+          output_element += input[get_i * input_m + get_j] * kernel[ki * kernel_m + kj];
+        }
+      }
+      output[i * input_m + j] = output_element;
     }
   }
-  return body_heights;
+}
+
+static constexpr std::array<float, 9> SMOOTH_SPHERE_BODY_HEIGHTS_KERNEL{1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f,
+                                                                        1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f,
+                                                                        1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f};
+void smooth_body_heights(std::span<float> body_heights, const size_t n_spheres, const size_t n, const size_t m) {
+  std::vector<float> smoothed(n * m);
+  for (size_t sphere_index = 0; sphere_index < n_spheres; ++sphere_index) {
+    auto sphere_span = body_heights.subspan(sphere_index * n * m, n * m);
+    cross_correlation(smoothed, sphere_span, SMOOTH_SPHERE_BODY_HEIGHTS_KERNEL, n, m);
+    cross_correlation(sphere_span, std::span(smoothed), SMOOTH_SPHERE_BODY_HEIGHTS_KERNEL, n, m);
+  }
 }
 
 State apply_container_collisions(State state, float restitution) {
@@ -116,8 +148,11 @@ State apply_container_collisions(State state, float restitution) {
   return state;
 };
 
-constexpr float GRAVITY = -9.81;
-
+static constexpr float GRAVITY = -9.81;
+static constexpr std::array<float, 9> NEIGHBOUR_KERNEL{
+    0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0,
+};
+static constexpr float NEIGHBOUR_KERNEL_SUM = 4;
 State apply_sphere_water_interaction(State state, const std::vector<float> &sphere_body_heights,
                                      const std::vector<float> &sphere_masses) {
   const size_t n_spheres = state._sphere_centers.size() / 3;
@@ -141,20 +176,8 @@ State apply_sphere_water_interaction(State state, const std::vector<float> &sphe
   }
   state._body_heights = body_heights;
 
-  constexpr float KERNEL_SUM = 4;
-  auto sums = cross_correlation(state._water_heights,
-                                {
-                                    0.0,
-                                    1.0,
-                                    0.0,
-                                    1.0,
-                                    0.0,
-                                    1.0,
-                                    0.0,
-                                    1.0,
-                                    0.0,
-                                },
-                                state._n, state._m, 3, 3);
+  std::vector<float> sums(state._water_heights.size());
+  cross_correlation(sums, state._water_heights, NEIGHBOUR_KERNEL, state._n, state._m);
 
   double wave_speed = std::min(state._wave_speed, 0.5 * state._spacing / state._time_delta);
   const float c = std::pow(wave_speed / state._spacing, 2);
@@ -164,8 +187,10 @@ State apply_sphere_water_interaction(State state, const std::vector<float> &sphe
   constexpr float VELOCITY_DAMPING = 0.3;
   auto velocity_damping = std::max(0.0, 1.0 - VELOCITY_DAMPING * state._time_delta);
   for (size_t index = 0; index < state._water_heights.size(); ++index) {
-    state._water_velocities[index] += state._time_delta * c * (sums[index] - KERNEL_SUM * state._water_heights[index]);
-    state._water_heights[index] += (sums[index] / KERNEL_SUM - state._water_heights[index]) * positional_damping;
+    state._water_velocities[index] +=
+        state._time_delta * c * (sums[index] - NEIGHBOUR_KERNEL_SUM * state._water_heights[index]);
+    state._water_heights[index] +=
+        (sums[index] / NEIGHBOUR_KERNEL_SUM - state._water_heights[index]) * positional_damping;
   }
 
   for (size_t index = 0; index < state._water_heights.size(); ++index) {
@@ -264,12 +289,11 @@ State step(const State &state) {
     new_state._sphere_centers[3 * sphere + 1] += new_state._time_delta * new_state._sphere_velocities[3 * sphere + 1];
   }
 
-  auto smoothed_sphere_body_heights =
-      smooth_sphere_body_heights(sphere_body_heights(new_state._sphere_centers, new_state._sphere_radii,
-                                                     new_state._water_xzs, new_state._water_heights),
-                                 n_spheres, new_state._n, new_state._m);
+  auto the_sphere_body_heights = sphere_body_heights(new_state._sphere_centers, new_state._sphere_radii,
+                                                     new_state._water_xzs, new_state._water_heights);
+  smooth_body_heights(the_sphere_body_heights, n_spheres, new_state._n, new_state._m);
 
-  new_state = apply_sphere_water_interaction(new_state, smoothed_sphere_body_heights, sphere_masses);
+  new_state = apply_sphere_water_interaction(new_state, the_sphere_body_heights, sphere_masses);
 
   constexpr float RESTITUTION = 0.1;
   new_state = apply_container_collisions(new_state, RESTITUTION);
